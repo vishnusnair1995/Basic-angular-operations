92f00b494f8737a49685e78e18f986b1987d81dc
unknown <vishnusnair5991@gmail.com>
2020-01-12 23:44:05 +0530

    sample angular-3:input decorator sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component,ViewChild, OnInit } from '@angular/core';
 import { Page1Component} from  './page1/page1.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   @ViewChild('page1')
   page1Component:Page1Component;
-
+  fromParent:any;
 passValue()
 {
 this.page1Component.parentValue="new value";
+}
+ngOnInit()
+{
+this.fromParent="parentValue"
 }
 }
--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page2',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page2.component.css']
 })
 export class Page2Component implements OnInit {
+  @Input()
+page2property:any="page2child";
 
   constructor() { }
 
